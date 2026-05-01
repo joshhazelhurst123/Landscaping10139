fbe0a3d776a011d7868e6e7e41327c7b5dcfe683
Josh <joshhazelhurst123@gmail.com>
2026-05-01 16:52:50 +1200

    Refactor App component to include BookingForm
    Removed API_URL and createBooking function. Integrated BookingForm component into App.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React from 'react';
-export const API_URL = "https://YOUR_API_ID.execute-api.YOUR_REGION.amazonaws.com/prod/bookings";
-
-export async function createBooking(data) {
-  const res = await fetch(API_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  });
-
-  return res.json();
-}
-
-
+import BookingForm from "./BookingForm";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Landscaping 101</h1>
+    <div className="app">
+      <BookingForm />
     </div>
   );
 }
