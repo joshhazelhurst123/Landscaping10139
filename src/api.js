66d58666850b6891003b83dd6645dfa0089459ad
Josh <joshhazelhurst123@gmail.com>
2026-05-17 23:03:45 +1200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 export async function createBooking(form) {
-  const res = await fetch("YOUR_BOOKING_LAMBDA_URL", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(form)
-  });
+  const res = await fetch(
+    "https://j10rrg72aa.execute-api.us-east-1.amazonaws.com/default/createBooking",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form)
+    }
+  );
 
   const raw = await res.json();
 
