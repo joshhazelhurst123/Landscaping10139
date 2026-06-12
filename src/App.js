52e4fbff0d501164a99113f9bb82feab8294fc0f
Josh <joshhazelhurst123@gmail.com>
2026-06-12 20:54:44 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookingForm from "./BookingForm";
 import Homepage from "./Homepage";
+import Services from "./Services";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+    <Route path="/services" element={<Services />} />
         <Route path="/bookings" element={<BookingForm />} />
       </Routes>
     </Router>
