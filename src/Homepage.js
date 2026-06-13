56f933478e12a0591819809da7d1d16bd1b6cb12
Josh <joshhazelhurst123@gmail.com>
2026-06-14 01:05:27 +1200

    Update contact information formatting in Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -23,8 +23,8 @@ export default function Homepage() {
           in keeping your property looking its best.
         </p>
     <p>
-    Wellington region New Zealand, 0221964920    
-    <br> joshhazelhurst123@gmail.com
+    Wellington region New Zealand, 0221964920,  
+     joshhazelhurst123@gmail.com
     </p>
 
         <a className="cta" href="/bookings">Book a Service</a>
