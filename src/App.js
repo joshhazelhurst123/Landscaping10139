6a07e122b3d222cc9ad3651f6c0fd32fce0533ff
Josh <joshhazelhurst123@gmail.com>
2026-06-14 20:14:55 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookingForm from "./BookingForm";
 import Homepage from "./Homepage";
 import Services from "./Services.js";
-import RefundPolicy from "./RefundPolicy.js";
+import RefundPolicy from "./RefundPolicy";
 
 function App() {
   return (
@@ -11,8 +11,14 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/services" element={<Services />} />
         <Route path="/bookings" element={<BookingForm />} />
+        {/* Primary correct route */}
+        <Route path="/refundpolicy" element={<RefundPolicy />} />
+        {/* Case‑insensitive fallbacks */}
         <Route path="/RefundPolicy" element={<RefundPolicy />} />
-        <Route path="/RefundPolicy/*" element={<RefundPolicy />} />
+        <Route path="/Refundpolicy" element={<RefundPolicy />} />
+        <Route path="/REFUNDPOLICY" element={<RefundPolicy />} />
+        {/* Optional: redirect dashed version */}
+        <Route path="/refund-policy" element={<RefundPolicy />} />
       </Routes>
     </Router>
   );
