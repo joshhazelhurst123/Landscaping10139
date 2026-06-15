de7131ce3d6a0f6711ddadb1a52b9887b46df42d
Josh <joshhazelhurst123@gmail.com>
2026-06-15 19:15:35 +1200

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -9,6 +9,7 @@ export default function Homepage() {
         <a href="/services">Services</a>
         <a href="/bookings">Bookings</a>
     <a href="/RefundPolicy">Refund Policy</a> 
+    <a href="/Terms">Terms & Conditions</a> 
       </nav>
 
       <header className="hero">
