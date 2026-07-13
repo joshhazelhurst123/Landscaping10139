3632dfdcfbbe9bfa138ad91f8fac71a0f2fc7b6f
Josh <joshhazelhurst123@gmail.com>
2026-07-13 21:02:38 +1200

    Update BookingForm.js

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -103,7 +103,7 @@ async function handleSubmit(e) {
         <a href="/Terms">Terms & Conditions</a>
       </nav>
 <p className="booking-price" style={{ textAlign: "center" }}>
-  <strong>$80.00 per hour</strong> — Wellington region only
+  <strong>$80.00 per hour</strong> Pricing applies to Wellington & Porirua region bookings.
 </p>
   <p className="booking-price">If you would like to make a booking, go to the Bookings page to make a booking, please find a available time slot submit your contact 
 details, including your phone number, email address, and name. Once you have made a booking, an email will be sent to you with a payment link. You will then need
